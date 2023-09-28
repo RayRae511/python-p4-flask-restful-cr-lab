ba9c419ffadbb1215a0d115608592498d1dfa8c3
Raymond Oeta <oetaray@gmail.com>
2023-09-28 20:32:53 +0300

    completed wrk

--- server/app.py
+++ server/app.py
@@ -15,12 +15,55 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+# Index Route
+@app.route('/plants', methods=['GET'])
+def get_plants():
+    plants = Plant.query.all()
+    plant_list = []
+    for plant in plants:
+        plant_list.append({
+            'id': plant.id,
+            'name': plant.name,
+            'image': plant.image,
+            'price': plant.price
+        })
+    return jsonify(plant_list)
 
-class Plants(Resource):
-    pass
+# Show Route
+@app.route('/plants/<int:id>', methods=['GET'])
+def get_plant(id):
+    plant = Plant.query.get(id)
+    if plant:
+        return jsonify({
+            'id': plant.id,
+            'name': plant.name,
+            'image': plant.image,
+            'price': plant.price
+        })
+    return jsonify({'message': 'Plant not found'}), 404
 
-class PlantByID(Resource):
-    pass
+# Create Route
+@app.route('/plants', methods=['POST'])
+def create_plant():
+    data = request.get_json()
+    name = data.get('name')
+    image = data.get('image')
+    price = data.get('price')
+
+    if not name or not image or not price:
+        return jsonify({'message': 'Invalid data'}), 400
+
+    new_plant = Plant(name=name, image=image, price=price)
+    db.session.add(new_plant)
+    db.session.commit()
+
+    return jsonify({
+        'id': new_plant.id,
+        'name': new_plant.name,
+        'image': new_plant.image,
+        'price': new_plant.price
+    }), 201
+
         
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,3 +7,11 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    price = db.Column(db.Float)
+
+    def __init__(self, name, image, price):
+        self.name = name
+        self.image = image
+        self.price = price
